7caadef0c3f7be7a5415ce39d91837384aa23fe6
Jay Barr <jayrbarr@gmail.com>
2017-10-15 14:12:42 -0400

    Markers highlight and animate on click

--- js/app.js
+++ js/app.js
@@ -4,249 +4,258 @@
 /* ======= Model ======= */
 
 var Restaurant = function (id, name, lat, lng, category) {
-    "use strict";
-    this.id = id;
-    this.name = name;
-    this.location = {
-        lat: lat,
-        lng: lng
-    };
-    this.category = category;
+	"use strict";
+	this.id = id;
+	this.name = name;
+	this.location = {
+		lat: lat,
+		lng: lng
+	};
+	this.category = category;
 };
 
 /* ======= ViewModel ======= */
 
 var ViewModel = function () {
 
-    "use strict";
-    var self = this;
-    var markers = [];
+	"use strict";
+	var self = this;
+	var markers = [];
 
-    self.restaurantList = ko.observableArray([
-        new Restaurant("43e1f592f964a520cc2e1fe3", "The Varsity", 33.771648596496604, -84.38931849661414, "American"),
-        new Restaurant("53a5f442498e192ea70e51bf", "Ladybird Grove & Mess Hall", 33.75968473532744, -84.36417242003067, "American"),
-        new Restaurant("4a47cde3f964a52023aa1fe3", "Mary Mac's Tea Room", 33.772755145900604, -84.37990628388033, "American"),
-        new Restaurant("4387a580f964a520062b1fe3", "The Vortex Bar & Grill", 33.779067072239236, -84.38439846038818, "American"),
-        new Restaurant("40e5f700f964a520100a1fe3", "Joe's on Juniper", 33.783213691244676, -84.38229560852051, "American"),
-        new Restaurant("4a54d8b7f964a5205bb31fe3", "Tin Drum Asian Kitchen", 33.825598, -84.364861, "Chinese"),
-        new Restaurant("4aa2f4e0f964a520bf4220e3", "Hsu's Chinese Food", 33.75956512767296, -84.38608767603405, "Chinese"),
-        new Restaurant("4a38371ef964a520cf9e1fe3", "Fortune Cookie", 33.82655117374547, -84.3328543209997, "Chinese"),
-        new Restaurant("4a9b2018f964a520803420e3", "Little Bangkok", 33.81585054624896, -84.3525194643487, "Chinese"),
-        new Restaurant("4a5a37d9f964a520ecb91fe3", "Chin Chin", 33.80255946152492, -84.4131821776038, "Chinese"),
-        new Restaurant("4af21075f964a52098e521e3", "Bistro Niko", 33.84654832641648, -84.36867751315688, "French"),
-        new Restaurant("546d31ad498e774b5c87fc8b", "Le Bilboquet", 33.83786774371116, -84.38049495279441, "French"),
-        new Restaurant("428a8580f964a52088231fe3", "Carroll Street Cafe", 33.74894384196275, -84.36804663948855, "French"),
-        new Restaurant("4ba04120f964a520686437e3", "The Sound Table", 33.754204706274386, -84.3718118100296, "French"),
-        new Restaurant("4c8ec3b5d68c6dcbd617ffa1", "Der Biergarten", 33.761880986766215, -84.3966481089592, "German"),
-        new Restaurant("50ee4908e4b0793e2a2814ac", "Crumley Burgers And Brewskis", 33.739655926716495, -84.38401304158316, "German"),
-        new Restaurant("50e4e4cae4b0c88a511f0f0e", "Summerhill Schnitzelhaus", 33.73968769076406, -84.38404878612383, "German")
-    ]);
+	self.restaurantList = ko.observableArray([
+		new Restaurant("43e1f592f964a520cc2e1fe3", "The Varsity", 33.771648596496604, -84.38931849661414, "American"),
+		new Restaurant("53a5f442498e192ea70e51bf", "Ladybird Grove & Mess Hall", 33.75968473532744, -84.36417242003067, "American"),
+		new Restaurant("4a47cde3f964a52023aa1fe3", "Mary Mac's Tea Room", 33.772755145900604, -84.37990628388033, "American"),
+		new Restaurant("4387a580f964a520062b1fe3", "The Vortex Bar & Grill", 33.779067072239236, -84.38439846038818, "American"),
+		new Restaurant("40e5f700f964a520100a1fe3", "Joe's on Juniper", 33.783213691244676, -84.38229560852051, "American"),
+		new Restaurant("4a54d8b7f964a5205bb31fe3", "Tin Drum Asian Kitchen", 33.825598, -84.364861, "Chinese"),
+		new Restaurant("4aa2f4e0f964a520bf4220e3", "Hsu's Chinese Food", 33.75956512767296, -84.38608767603405, "Chinese"),
+		new Restaurant("4a38371ef964a520cf9e1fe3", "Fortune Cookie", 33.82655117374547, -84.3328543209997, "Chinese"),
+		new Restaurant("4a9b2018f964a520803420e3", "Little Bangkok", 33.81585054624896, -84.3525194643487, "Chinese"),
+		new Restaurant("4a5a37d9f964a520ecb91fe3", "Chin Chin", 33.80255946152492, -84.4131821776038, "Chinese"),
+		new Restaurant("4af21075f964a52098e521e3", "Bistro Niko", 33.84654832641648, -84.36867751315688, "French"),
+		new Restaurant("546d31ad498e774b5c87fc8b", "Le Bilboquet", 33.83786774371116, -84.38049495279441, "French"),
+		new Restaurant("428a8580f964a52088231fe3", "Carroll Street Cafe", 33.74894384196275, -84.36804663948855, "French"),
+		new Restaurant("4ba04120f964a520686437e3", "The Sound Table", 33.754204706274386, -84.3718118100296, "French"),
+		new Restaurant("4c8ec3b5d68c6dcbd617ffa1", "Der Biergarten", 33.761880986766215, -84.3966481089592, "German"),
+		new Restaurant("50ee4908e4b0793e2a2814ac", "Crumley Burgers And Brewskis", 33.739655926716495, -84.38401304158316, "German"),
+		new Restaurant("50e4e4cae4b0c88a511f0f0e", "Summerhill Schnitzelhaus", 33.73968769076406, -84.38404878612383, "German")
+	]);
 
-    self.currentRestaurant = ko.observable(self.restaurantList()[0]);
+	self.currentRestaurant = ko.observable(self.restaurantList()[0]);
 
-    self.cuisines = ["All", "American", "Chinese", "French", "German"];
+	self.cuisines = ["All", "American", "Chinese", "French", "German"];
 
-    self.selectedCuisine = ko.observable(self.cuisines[0]);
+	self.selectedCuisine = ko.observable(self.cuisines[0]);
 
-    self.filteredRestaurants = ko.computed(function () {
-        if (self.selectedCuisine() === self.cuisines[0]) {
-            return self.restaurantList();
-        } else {
-            return ko.utils.arrayFilter(self.restaurantList(), function (rest) {
-                return rest.category === self.selectedCuisine();
-            });
-        }
-    });
+	self.filteredRestaurants = ko.computed(function () {
+		if (self.selectedCuisine() === self.cuisines[0]) {
+			return self.restaurantList();
+		} else {
+			return ko.utils.arrayFilter(self.restaurantList(), function (rest) {
+				return rest.category === self.selectedCuisine();
+			});
+		}
+	});
 
-    self.selectedCuisine.subscribe(function (newCuisine) {
-        if (newCuisine === self.cuisines[0]) {
-            self.displayMarkers(0, 16);
-        } else if (newCuisine === self.cuisines[1]) {
-            self.displayMarkers(0, 4);
-        } else if (newCuisine === self.cuisines[2]) {
-            self.displayMarkers(5, 9);
-        } else if (newCuisine === self.cuisines[3]) {
-            self.displayMarkers(10, 13);
-        } else if (newCuisine === self.cuisines[4]) {
-            self.displayMarkers(14, 16);
-        }
-    });
+	self.selectedCuisine.subscribe(function (newCuisine) {
+		if (newCuisine === self.cuisines[0]) {
+			self.displayMarkers(0, 16);
+		} else if (newCuisine === self.cuisines[1]) {
+			self.displayMarkers(0, 4);
+		} else if (newCuisine === self.cuisines[2]) {
+			self.displayMarkers(5, 9);
+		} else if (newCuisine === self.cuisines[3]) {
+			self.displayMarkers(10, 13);
+		} else if (newCuisine === self.cuisines[4]) {
+			self.displayMarkers(14, 16);
+		}
+	});
 
-    self.displayMarkers = function (start, stop) {
-        if (markers.length === 0) {
-            return true;
-        }
-        for (var i = 0; i < start; i++) {
-            markers[i].setMap(null);
-        }
-        for (i = start; i <= stop; i++) {
-            markers[i].setMap(this.map);
-        }
-        for (i = stop + 1; i < 17; i++) {
-            markers[i].setMap(null);
-        }
-    };
+	self.displayMarkers = function (start, stop) {
+		if (markers.length === 0) {
+			return true;
+		}
+		for (var i = 0; i < start; i++) {
+			markers[i].setMap(null);
+		}
+		for (i = start; i <= stop; i++) {
+			markers[i].setMap(this.map);
+		}
+		for (i = stop + 1; i < 17; i++) {
+			markers[i].setMap(null);
+		}
+	};
 
-    /* ======= View: Map ======= */
+	/* ======= View: Map ======= */
 
-    self.initMap = function () {
-        var styles = [{
-            featureType: 'water',
-            stylers: [{
-                color: '#19a0d8'
-            }]
-        }, {
-            featureType: 'administrative',
-            elementType: 'labels.text.stroke',
-            stylers: [{
-                color: '#ffffff'
-            }, {
-                weight: 6
-            }]
-        }, {
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#e85113'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{
-                color: '#efe9e4'
-            }, {
-                lightness: -40
-            }]
-        }, {
-            featureType: 'transit.station',
-            stylers: [{
-                weight: 9
-            }, {
-                hue: '#e85113'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'labels.icon',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }, {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{
-                lightness: 100
-            }]
-        }, {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                lightness: -100
-            }]
-        }, {
-            featureType: 'poi',
-            elementType: 'geometry',
-            stylers: [{
-                visibility: 'on'
-            }, {
-                color: '#f0e4d3'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [{
-                color: '#efe9e4'
-            }, {
-                lightness: -25
-            }]
-        }];
+	self.initMap = function () {
+		var styles = [{
+			featureType: 'water',
+			stylers: [{
+				color: '#19a0d8'
+			}]
+		}, {
+			featureType: 'administrative',
+			elementType: 'labels.text.stroke',
+			stylers: [{
+				color: '#ffffff'
+			}, {
+				weight: 6
+			}]
+		}, {
+			featureType: 'administrative',
+			elementType: 'labels.text.fill',
+			stylers: [{
+				color: '#e85113'
+			}]
+		}, {
+			featureType: 'road.highway',
+			elementType: 'geometry.stroke',
+			stylers: [{
+				color: '#efe9e4'
+			}, {
+				lightness: -40
+			}]
+		}, {
+			featureType: 'transit.station',
+			stylers: [{
+				weight: 9
+			}, {
+				hue: '#e85113'
+			}]
+		}, {
+			featureType: 'road.highway',
+			elementType: 'labels.icon',
+			stylers: [{
+				visibility: 'off'
+			}]
+		}, {
+			featureType: 'water',
+			elementType: 'labels.text.stroke',
+			stylers: [{
+				lightness: 100
+			}]
+		}, {
+			featureType: 'water',
+			elementType: 'labels.text.fill',
+			stylers: [{
+				lightness: -100
+			}]
+		}, {
+			featureType: 'poi',
+			elementType: 'geometry',
+			stylers: [{
+				visibility: 'on'
+			}, {
+				color: '#f0e4d3'
+			}]
+		}, {
+			featureType: 'road.highway',
+			elementType: 'geometry.fill',
+			stylers: [{
+				color: '#efe9e4'
+			}, {
+				lightness: -25
+			}]
+		}];
 
-        // Constructor creates a new map - only center and zoom are required.
-        self.map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 33.7582458,
-                lng: -84.3885123
-            },
-            zoom: 15,
-            styles: styles,
-            mapTypeControl: false
-        });
-        self.largeInfowindow = new google.maps.InfoWindow();
-        var defaultIcon = self.makeMarkerIcon('0091ff');
-        var len = self.restaurantList().length;
-        if (len > 0) {
-            var bounds = new google.maps.LatLngBounds();
-            for (var i = 0; i < len; i++) {
-                var position = self.restaurantList()[i].location;
-                var title = self.restaurantList()[i].name;
-                var marker = new google.maps.Marker({
-                    position: position,
-                    title: title,
-                    animation: google.maps.Animation.DROP,
-                    icon: defaultIcon,
-                    id: i
-                });
-                marker.setMap(this.map);
-                markers.push(marker);
-                marker.addListener('click', function () {
-                    self.populateInfoWindow(this, self.largeInfowindow);
-                });
-                bounds.extend(marker.position);
-            }
-            this.map.fitBounds(bounds);
-        }
-    };
+		// Constructor creates a new map - only center and zoom are required.
+		self.map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: 33.7582458,
+				lng: -84.3885123
+			},
+			zoom: 15,
+			styles: styles,
+			mapTypeControl: false
+		});
+		self.largeInfowindow = new google.maps.InfoWindow();
+		self.defaultIcon = self.makeMarkerIcon('0091ff');
+		var highlightedIcon = self.makeMarkerIcon('FFFF24');
+		var len = self.restaurantList().length;
+		if (len > 0) {
+			var bounds = new google.maps.LatLngBounds();
+			for (var i = 0; i < len; i++) {
+				var position = self.restaurantList()[i].location;
+				var title = self.restaurantList()[i].name;
+				var marker = new google.maps.Marker({
+					position: position,
+					title: title,
+					animation: google.maps.Animation.DROP,
+					icon: self.defaultIcon,
+					id: i
+				});
+				marker.setMap(this.map);
+				markers.push(marker);
+				marker.addListener('click', function () {
+					for (i = 0; i < len; i++) {
+							markers[i].setAnimation(null);
+							markers[i].setIcon(self.defaultIcon);
+						}
+					self.populateInfoWindow(this, self.largeInfowindow);
+					this.setIcon(highlightedIcon);
+					this.setAnimation(google.maps.Animation.BOUNCE);
+				});
+				bounds.extend(marker.position);
+			}
+			this.map.fitBounds(bounds);
+		}
+	};
 
-    // This function takes in a COLOR, and then creates a new marker
-    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-    // of 0, 0 and be anchored at 10, 34).
-    self.makeMarkerIcon = function (markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-            '|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21, 34));
-        return markerImage;
-    };
-    
-    // This function populates the infowindow when the marker is clicked. We'll only allow
-    // one infowindow which will open at the marker that is clicked, and populate based
-    // on that markers position.
-    self.populateInfoWindow = function (marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker !== marker) {
-            // Clear the infowindow content to give the streetview time to load.
-            infowindow.setContent('');
-            infowindow.marker = marker;
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function () {
-                infowindow.marker = null;
-            });
-            var restaurantID = self.restaurantList()[marker.id].id;
-            var foursquarePrefix = "https://api.foursquare.com/v2/venues/";
-            var foursquareSuffix = "/photos";
-            $.getJSON(foursquarePrefix+restaurantID+foursquareSuffix,{
-                limit: 1,
-                v: "20170801",
-                client_id:"PP3RWERUTIA1OI3DNHTIKX4T2WSJ0L1UEXUXPEZ1Z2BFY2RM",
-                client_secret:"FNI4MCR2USHA21HA4V0RHS3SFHZ4GXSHLMZACQESYYUKAODT"
-            }, function(data){
-                if (data.response.photos.count === 0) {
-                    infowindow.setContent('<div>' + marker.title + '</div>' +
-                        '<div>No Image Found</div>');
-                } else {
-                    var imagePrefix = data.response.photos.items[0].prefix;
-                    var imageSuffix = data.response.photos.items[0].suffix;
-                    var size = "300x300";
-                    var credit = data.response.photos.items[0].source.name;
-                    var creditUrl = data.response.photos.items[0].source.url;
-                    infowindow.setContent('<div class="infoWindow"><p>'+marker.title+'</p><div><img src="'+imagePrefix+size+imageSuffix+'" alt="'+marker.title+' picture" ></div><p>Image credit: <a href="'+creditUrl+'">'+credit+'</a></p></div>');
-                }
-            });
-            infowindow.open(self.map,marker);
-        }
-    };
+	// This function takes in a COLOR, and then creates a new marker
+	// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+	// of 0, 0 and be anchored at 10, 34).
+	self.makeMarkerIcon = function (markerColor) {
+		var markerImage = new google.maps.MarkerImage(
+			'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+			'|40|_|%E2%80%A2',
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0, 0),
+			new google.maps.Point(10, 34),
+			new google.maps.Size(21, 34));
+		return markerImage;
+	};
+
+	// This function populates the infowindow when the marker is clicked. We'll only allow
+	// one infowindow which will open at the marker that is clicked, and populate based
+	// on that markers position.
+	self.populateInfoWindow = function (marker, infowindow) {
+		// Check to make sure the infowindow is not already opened on this marker.
+		if (infowindow.marker !== marker) {
+			// Clear the infowindow content to give the streetview time to load.
+			infowindow.setContent('');
+			infowindow.marker = marker;
+			// Make sure the marker property is cleared if the infowindow is closed.
+			infowindow.addListener('closeclick', function () {
+				infowindow.marker = null;
+				marker.setIcon(self.defaultIcon);
+				marker.setAnimation(null);
+			});
+			var restaurantID = self.restaurantList()[marker.id].id;
+			var foursquarePrefix = "https://api.foursquare.com/v2/venues/";
+			var foursquareSuffix = "/photos";
+			$.getJSON(foursquarePrefix + restaurantID + foursquareSuffix, {
+				limit: 1,
+				v: "20170801",
+				client_id: "PP3RWERUTIA1OI3DNHTIKX4T2WSJ0L1UEXUXPEZ1Z2BFY2RM",
+				client_secret: "FNI4MCR2USHA21HA4V0RHS3SFHZ4GXSHLMZACQESYYUKAODT"
+			}, function (data) {
+				if (data.response.photos.count === 0) {
+					infowindow.setContent('<div>' + marker.title + '</div>' +
+						'<div>No Image Found</div>');
+				} else {
+					var imagePrefix = data.response.photos.items[0].prefix;
+					var imageSuffix = data.response.photos.items[0].suffix;
+					var size = "300x300";
+					var credit = data.response.photos.items[0].source.name;
+					var creditUrl = data.response.photos.items[0].source.url;
+					infowindow.setContent('<div class="infoWindow"><p>' + marker.title + '</p><div><img src="' + imagePrefix + size + imageSuffix + '" alt="' + marker.title + ' picture" ></div><p>Image credit: <a href="' + creditUrl + '">' + credit + '</a></p></div>');
+				}
+			});
+			infowindow.open(self.map, marker);
+		}
+	};
 };
 
 // make it go!
