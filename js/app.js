29d154198a76310f4ed820bf146ebf9472351446
Jay Barr <jayrbarr@gmail.com>
2017-10-19 12:27:59 -0400

    Added marker to Restaurant object. Removed markers array.

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ var Restaurant = function (data) {
         lng: data.lng
     };
     this.category = data.category;
+    this.marker = data.marker;
     this.selected = ko.observable(false);
 };
 
@@ -37,9 +38,6 @@ var ViewModel = function () {
 
     // preserve the ViewModel binding context as a variable
     var self = this;
-
-    // global array to hold map markers for ViewModel and Maps API
-    var markers = [];
 
     // create empty KnockoutJS ObservableArray to hold Restaurants
     this.restaurantList = ko.observableArray([]);
@@ -77,22 +75,6 @@ var ViewModel = function () {
         }
     });
 
-    // Sets KnockoutJS subscribe on selectedCuisine to 
-    // display the associated map markers for selected cuisine.
-    this.selectedCuisine.subscribe(function (newCuisine) {
-        if (newCuisine === self.cuisines()[0]) {
-            self.displayMarkers(0, 16);
-        } else if (newCuisine === self.cuisines()[1]) {
-            self.displayMarkers(0, 4);
-        } else if (newCuisine === self.cuisines()[2]) {
-            self.displayMarkers(5, 9);
-        } else if (newCuisine === self.cuisines()[3]) {
-            self.displayMarkers(10, 13);
-        } else if (newCuisine === self.cuisines()[4]) {
-            self.displayMarkers(14, 16);
-        }
-    });
-
     // First, clears any previously set highlight on restaurantList
     // by setting selected to false on all Restaurants. Then,
     // sets selected to true on data-bind click Restaurant.
@@ -104,17 +86,20 @@ var ViewModel = function () {
         }
         self.currentRestaurant(restaurant);
         self.currentRestaurant().selected(true);
-        var index = self.restaurantList().indexOf(self.currentRestaurant());
-        self.populateInfoWindow(markers[index], self.largeInfowindow);
+        mapView.populateInfoWindow(self.currentRestaurant().marker);
     };
+};
 
-    /* =======  Map =======
-     * Functions for Google Maps API to control Google Map and
-     * map markers for app.
-     */
+/* =======  View: Map =======
+ * Functions for Google Maps API to control Google Map and
+ * map markers for app.
+ */
 
+var mapView = {
+    
     // Callback function for Google Maps API initialization
-    this.initMap = function () {
+    initMap: function () {
+        "use strict";
         // Styles array to control map appearance.
         // Used below in initial maps call.
         // Adapted from course code, New York Listings
@@ -189,7 +174,7 @@ var ViewModel = function () {
         }];
 
         // Constructor creates a new map - only center and zoom are required.
-        self.map = new google.maps.Map(document.getElementById('map'), {
+        this.map = new google.maps.Map(document.getElementById('map'), {
             center: {
                 lat: 33.7582458, // downtown Atlanta
                 lng: -84.3885123
@@ -200,12 +185,12 @@ var ViewModel = function () {
         });
 
         // Create elements for marker creation and highlighting
-        self.largeInfowindow = new google.maps.InfoWindow();
-        self.defaultIcon = self.makeMarkerIcon('0091ff');
-        self.highlightedIcon = self.makeMarkerIcon('FFFF24');
+        this.largeInfowindow = new google.maps.InfoWindow();
+        this.defaultIcon = mapView.makeMarkerIcon('0091ff');
+        this.highlightedIcon = mapView.makeMarkerIcon('FFFF24');
 
         // restaurantList length determines number of markers needed
-        var len = self.restaurantList().length;
+        var len = vm.restaurantList().length;
 
         // make sure we have restaurants
         if (len > 0) {
@@ -213,39 +198,41 @@ var ViewModel = function () {
             var bounds = new google.maps.LatLngBounds();
             // create a marker for each Restaurant from restaurantList
             for (var i = 0; i < len; i++) {
-                var position = self.restaurantList()[i].location;
-                var title = self.restaurantList()[i].name;
+                var position = vm.restaurantList()[i].location;
+                var title = vm.restaurantList()[i].name;
                 var marker = new google.maps.Marker({
                     position: position,
                     title: title,
                     animation: google.maps.Animation.DROP,
-                    icon: self.defaultIcon,
+                    icon: this.defaultIcon,
                     id: i
                 });
                 marker.setMap(this.map); // put marker on map
-                markers.push(marker); // add marker to global markers array
+                vm.restaurantList()[i].marker = marker; // add marker to Restaurant object
 
                 // set click listener for each marker id
-                this.markerClick(marker);
+                mapView.markerClick(marker);
 
                 // if necessary, extend bounds to fit markers created
                 bounds.extend(marker.position);
             }
             this.map.fitBounds(bounds);
         }
-    };
+    },
 
-    this.markerClick = function (markerCopy) {
+    markerClick: function (markerCopy) {
+        "use strict";
         markerCopy.addListener('click', function () {
-            self.highlightRestaurant(self.restaurantList()[markerCopy.id]);
+            vm.highlightRestaurant(vm.restaurantList()[markerCopy.id]);
         });
-    };
+    },
 
     // This function takes in a COLOR, and then creates a new marker
     // icon of that color. The icon will be 21 px wide by 34 high, have an origin
     // of 0, 0 and be anchored at 10, 34).
     // Adapted from course code, New York Listings
-    this.makeMarkerIcon = function (markerColor) {
+    makeMarkerIcon: function (markerColor) {
+        "use strict";
         var markerImage = new google.maps.MarkerImage(
             'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
             '|40|_|%E2%80%A2',
@@ -254,45 +241,27 @@ var ViewModel = function () {
             new google.maps.Point(10, 34),
             new google.maps.Size(21, 34));
         return markerImage;
-    };
-
-    // Displays markers in a range from start to stop. Markers array index
-    // matches restaurantList array index. So each marker corresponds to
-    // Restaurant from restaurantList. Range should capture Restaurants
-    // matching a cuisine type. Therefore, all Restaurants need to be sorted
-    // by cuisine in restaurantList to have simple range for cuisine type.
-    this.displayMarkers = function (start, stop) {
-        if (markers.length === 0) {
-            return true;
-        }
-        for (var i = 0; i < start; i++) {
-            markers[i].setMap(null);
-        }
-        for (i = start; i <= stop; i++) {
-            markers[i].setMap(this.map);
-        }
-        for (i = stop + 1; i < 17; i++) {
-            markers[i].setMap(null);
-        }
-    };
+    },
 
     // Clears all animation and highlighting from all markers.
-    this.clearMarkers = function () {
-        var len = self.restaurantList().length;
-        for (var i = 0; i < len; i++) {
-            markers[i].setAnimation(null);
-            markers[i].setIcon(self.defaultIcon);
-        }
-    };
+    clearMarkers: function () {
+        "use strict";
+        vm.restaurantList().forEach(function(restaurant) {
+            restaurant.marker.setAnimation(null);
+            restaurant.marker.setIcon(self.defaultIcon);
+        });
+    },
 
     // This function populates the infowindow when the marker is clicked. We'll only allow
     // one infowindow which will open at the marker that is clicked, and populate based
     // on that markers position.
-    this.populateInfoWindow = function (marker, infowindow) {
+    populateInfoWindow: function (marker) {
+        "use strict";
+        var infowindow = new google.maps.InfoWindow();
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker !== marker) {
-            self.clearMarkers();
-            marker.setIcon(self.highlightedIcon);
+            mapView.clearMarkers();
+            marker.setIcon(mapView.highlightedIcon);
             marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function () {
                 marker.setAnimation(null)
@@ -303,13 +272,13 @@ var ViewModel = function () {
             // Make sure the marker property is cleared if the infowindow is closed.
             infowindow.addListener('closeclick', function () {
                 infowindow.marker = null;
-                marker.setIcon(self.defaultIcon);
+                marker.setIcon(mapView.defaultIcon);
                 marker.setAnimation(null);
-                self.currentRestaurant().selected(false);
+                vm.currentRestaurant().selected(false);
             });
 
             // prepare elements for JSON get from Foursquare API to fetch photo
-            var restaurantID = self.restaurantList()[marker.id].id;
+            var restaurantID = vm.restaurantList()[marker.id].id;
             var foursquarePrefix = "https://api.foursquare.com/v2/venues/";
             var foursquareSuffix = "/photos";
 
@@ -340,9 +309,9 @@ var ViewModel = function () {
                         '<div>No Image Found</div>');
                     alert("Unable to retrieve photo.");
                 });
-            infowindow.open(self.map, marker); // open infoWindow on associated marker
+            infowindow.open(vm.map, marker); // open infoWindow on associated marker
         }
-    };
+    }
 };
 
 // make it go!
